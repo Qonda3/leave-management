b811776bff00132530fab962f366e726ea8aea62
Qonda <138157862+Qonda3@users.noreply.github.com>
2026-05-09 21:53:46 +0200

    Add LeaveType model with mass assignable attributes and relationships for leave requests and balances

--- app/Models/LeaveType.php
+++ app/Models/LeaveType.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LeaveType extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name', 
+        'default_days', 
+        'description'
+    ];
+
+    /**
+     * Get the leave requests associated with this leave type.
+     */
+    public function leaveRequest()
+    {
+        return $this->hasMany(LeaveRequet::class);
+    }
+
+    /**
+     * Get the leave balances associated with this leave type.
+     */
+    public function leaveBalances()
+    {
+        return $this->hasMany(LeaveBalance::class);
+    }
 }
