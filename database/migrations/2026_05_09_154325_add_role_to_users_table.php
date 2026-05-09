909bfcf37308d40525f01962791f292f98961d01
Qonda <138157862+Qonda3@users.noreply.github.com>
2026-05-09 18:41:18 +0200

    Set default value for role column in users table migration

--- database/migrations/2026_05_09_154325_add_role_to_users_table.php
+++ database/migrations/2026_05_09_154325_add_role_to_users_table.php
@@ -9,7 +9,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['employee', 'manager']);
+            $table->enum('role', ['employee', 'manager'])
+                  ->default('employee');
         });
     }
 
