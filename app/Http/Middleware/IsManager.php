2a6fbbe60c5551d4c1e4dd0ab2085a56e09920a2
Qonda <138157862+Qonda3@users.noreply.github.com>
2026-05-11 08:50:07 +0200

    Remove unnecessary inheritance from Middleware in IsManager class

--- app/Http/Middleware/IsManager.php
+++ app/Http/Middleware/IsManager.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class IsManager extends Middleware
+class IsManager
 {
     /**
      * Handle an incoming request.
