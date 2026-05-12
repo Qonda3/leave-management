f9368cd1d5a6784508daf84e56232804d85d7456
Qonda <138157862+Qonda3@users.noreply.github.com>
2026-05-12 13:26:41 +0200

    Add manager dashboard view to display employee leave requests

---
+++ resources/views/dashboard/manager.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Manager Dashboard')
+
+@section('content')
+
+<div>
+    <h1>
+        Manager Dashboard
+    </h1>
+
+    <p>
+        Review employee leave requests.
+    </p>
+</div>
+
+@endsection
