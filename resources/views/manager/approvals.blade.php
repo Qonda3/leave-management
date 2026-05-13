2c1e741fdad4038b93bb0ef217780021f7d1013c
Qonda <138157862+Qonda3@users.noreply.github.com>
2026-05-13 07:22:39 +0200

    Add leave approvals view for manager dashboard

---
+++ resources/views/manager/approvals.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Leave Approvals')
+
+@section('content')
+
+<div>
+    <h1>
+        Leave Approvals
+    </h1>
+
+    <p>
+        Review employee leave requests.
+    </p>
+</div>
+
+@endsection
