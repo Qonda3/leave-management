b4a695cfee561e62cf53b5f59b3def4776cdfd97
Qonda <138157862+Qonda3@users.noreply.github.com>
2026-05-20 22:03:56 +0200

    Add leave requests index view template

---
+++ resources/views/leaves/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'My Leave Requests')
+
+@section('content')
+
+<div>
+    <h1>
+        My Leave Requests
+    </h1>
+
+    <p>
+        Track your leave applications.
+    </p>
+</div>
+
+@endsection
